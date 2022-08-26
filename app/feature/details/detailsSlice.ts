d49f716371777cbc24cfed9689ddb8a2c8c49b2b
Ardhendu Pramanik <ardhendujj002@gmail.com>
2022-08-26 10:21:54 +0530

    remove unnecessary code

--- app/feature/details/detailsSlice.ts
+++ app/feature/details/detailsSlice.ts
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "../../store";
-import { valueProps, reqProps, jobProps, interviewProps } from "../../../model";
-import { stat } from "fs";
+
+import { valueProps} from "../../../model";
+
 
 // Define the initial state using that type
 const initialState: valueProps = {
